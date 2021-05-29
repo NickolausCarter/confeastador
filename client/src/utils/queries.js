6e5797f83895dc47187ab0821f1a49e4e22b5e9f
Pure KaoZ <nickolaus.carter81@gmail.com>
2021-05-29 13:53:29 -0500

    added service worker and modified manifest

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,7 @@
-export const QUERY_RESTAURANT = gql`
-  {
-    restaurants: [Restaurant]
-  }
-`;
+// import gql from "graphql-tag";
+
+// export const QUERY_RESTAURANT = gql`
+//   {
+//     restaurants: [Restaurant]
+//   }
+// `;
